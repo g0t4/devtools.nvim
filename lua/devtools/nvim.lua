b2865b4c6733a8188b386da64a2181b385fe503a
Wes Higbee <wes.mcclure@gmail.com>
2025-05-26 18:01:15 -0500

    swap in super_iter

--- lua/devtools/nvim.lua
+++ lua/devtools/nvim.lua
@@ -1,4 +1,5 @@
 local messages = require('devtools.messages')
+local super_iter = require('devtools.super_iter')
 
 local M = {}
 
@@ -86,7 +87,7 @@ function M.dump_keymaps_sorted_by_lhs(lhs_starts_with)
 
     -- :append(vim.api.nvim_buf_get_keymap(0, 'n')) -- TODO! Buffer-local keymaps
     -- TODO! imap/vmap/cmap, etc
-    local n_maps = vim.iter(vim.api.nvim_get_keymap('n'))
+    local n_maps = super_iter(vim.api.nvim_get_keymap('n'))
 
     maps = n_maps
         :map(function(m)
