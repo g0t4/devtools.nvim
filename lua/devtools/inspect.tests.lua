288eb0c1d36f95fff36a636f08a3b567b579293e
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 10:03:04 -0500

    make inspect tests work... seem like manual exploratory tests but just use them

--- lua/devtools/inspect.tests.lua
+++ lua/devtools/inspect.tests.lua
@@ -1,12 +1,21 @@
-require("devtools.inspect")
+local inspect = require("devtools.inspect")
 
--- lists:
-assert(tbl_is_list({ 1, 2, 3 }))
-assert(tbl_is_list({}))
--- not lists:
-assert(tbl_is_list({ a = 1, b = 2, [3] = 4 }) == false)
+describe("exploratory", function()
+    it("vim.islist", function()
+        --
+        -- lists:
+        assert(vim.islist({ 1, 2, 3 }))
+        assert(vim.islist({}))
+        -- not lists:
+        assert(vim.islist({ a = 1, b = 2, [3] = 4 }) == false)
+    end)
 
-inspect({}) -- "{ }"
-inspect({ 1, 2, 3 }) --  == "{ 1, 2, 3, }"
-inspect({ a = 1, b = 2, [3] = 4 })
-inspect({ a = 'foo" the bar' })
+    it("inspect", function()
+        -- TODO add assertions and make into real tests?
+        -- TODO flesh out more inspect testing!
+        inspect({})          -- "{ }"
+        inspect({ 1, 2, 3 }) --  == "{ 1, 2, 3, }"
+        inspect({ a = 1, b = 2, [3] = 4 })
+        inspect({ a = 'foo" the bar' })
+    end)
+end)
