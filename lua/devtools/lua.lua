958d81aa846f161d26d35860e0d95286deda02a4
Wes Higbee <wes.mcclure@gmail.com>
2025-05-31 21:33:32 -0500

    command to query packages.loaded with pattern

--- lua/devtools/lua.lua
+++ lua/devtools/lua.lua
@@ -1,5 +1,7 @@
 local inspect = require("devtools.inspect")
 local messages = require("devtools.messages")
+local super_iter = require("devtools.super_iter")
+
 local M = {}
 
 -- vim.print(_G) is useful to inspect modules
@@ -8,11 +10,31 @@ function M.dump_globals()
     messages.append(vim.inspect(_G))
 end
 
+function M.dump_packages_loaded(name_pattern)
+    local names = super_iter(package.loaded):map(function(k, _)
+            return k
+        end)
+        :filter(function(k) return name_pattern == nil or k:match(name_pattern) end)
+        :sort() -- todo option to control this so I can see order loaded vs by name?
+        :join("\n")
+
+    messages.ensure_open()
+    messages.append("Loaded packages:\n" .. names)
+end
+
 function M.setup()
     -- FYI use :buffers  ... builtin command with good details about each buffer
     -- for now, no command, just really wanted the function reminder
     -- and then eventually find a better way to inspect _G (globals)
-    -- vim.api.nvim_create_user_command("DumpGlobals", M.dump_globals, {})
+    vim.api.nvim_create_user_command("DevtoolsDumpGlobals", M.dump_globals, {})
+    vim.api.nvim_create_user_command("DevtoolsDumpPackagesLoaded", function(args)
+        -- USAGE:
+        --   :DevtoolsDumpPackagesLoaded ^vim.lsp
+
+        local name_pattern = args.fargs[1]
+        print("filter: " .. tostring(name_pattern))
+        return M.dump_packages_loaded(name_pattern)
+    end, { nargs = "*" })
 end
 
 return M
