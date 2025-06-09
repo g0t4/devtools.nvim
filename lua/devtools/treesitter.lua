3892740b8dc0b5ff03459fc42a713dc0bdecf80e
Wes Higbee <wes.mcclure@gmail.com>
2025-06-09 08:05:50 -0500

    lua example moved

--- lua/devtools/treesitter.lua
+++ lua/devtools/treesitter.lua
@@ -22,6 +22,7 @@ local query_string = [[
 ]]
 
 function M.get_static_requires_lua(bufnr)
+    messages.ensure_open()
     bufnr = bufnr or vim.api.nvim_get_current_buf()
     local parser = ts.get_parser(bufnr, "lua")
     local tree = parser:parse()[1]
@@ -33,15 +34,27 @@ function M.get_static_requires_lua(bufnr)
     for pattern, match, metadata in query:iter_matches(root, bufnr) do
         for id, nodes in pairs(match) do
             local name = query.captures[id]
-            messages.append(name)
             for id, node in ipairs(nodes) do
                 local name = query.captures[id]
-                messages.append(name)
+                -- messages.append(name)
                 M.inspect_ts(node)
                 -- messages.append(lua.inspect_userdata(node))
             end
         end
     end
+
+    -- -- lua example of require
+    -- (chunk ; [0, 0] - [26, 0]
+    --   local_declaration: (variable_declaration ; [0, 0] - [0, 45]
+    --     (assignment_statement ; [0, 6] - [0, 45]
+    --       (variable_list ; [0, 6] - [0, 14]
+    --         name: (identifier)) ; [0, 6] - [0, 14]
+    --       (expression_list ; [0, 17] - [0, 45]
+    --         value: (function_call ; [0, 17] - [0, 45]
+    --           name: (identifier) ; [0, 17] - [0, 24]
+    --           arguments: (arguments ; [0, 24] - [0, 45]
+    --             (string ; [0, 25] - [0, 44]
+    --               content: (string_content))))))) ; [0, 26] - [0, 43]
 
 
     return results
