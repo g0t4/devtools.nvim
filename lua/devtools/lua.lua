7bd7036e1cfe8fd0e1e2389a0004fd6c11133d76
Wes Higbee <wes.mcclure@gmail.com>
2025-06-08 23:25:57 -0500

    move lua function inspect code to lua helper module

--- lua/devtools/lua.lua
+++ lua/devtools/lua.lua
@@ -4,6 +4,53 @@ local super_iter = require("devtools.super_iter")
 
 local M = {}
 
+function M.inspect_fn(fn)
+    -- debug return type: https://www.lua.org/manual/5.1/manual.html#3.8
+    local info = debug.getinfo(fn, "nS") -- n = name, S = source info
+    local what = info.what
+    if what == "Lua" then
+        -- not that useful to show "Lua"
+        what = nil
+    end
+    local source_or_line = M.inspect_fn_source_or_line(info)
+    -- BTW by logging entire object, its clear what is what, field wise...
+    --   AND, if a field is nil it won't take up any space!
+    return {
+        name = info.name,
+        what = what,      -- "Lua", "C", "main", etc.
+        func = info.func, -- name of function or nil
+        source_line = source_or_line
+    }
+end
+
+function M.inspect_fn_source_or_line(info)
+    local source_line = info.source .. ":" .. tostring(info.linedefined)
+    if not string.find(source_line, "^%@") then
+        -- return as is
+        return info.source
+    end
+    source_line = source_line:gsub("^@", "") -- strip '@' from filename
+
+    -- to save space, look for cwd
+    --  i.e. cwd = /foo/bar/
+    --       source = /foo/bar/bam/test.lua
+    --       =>       bam/test.lua
+    local cwd = vim.fn.getcwd()                               -- PRN pass cwd and home, if expensive to lookup on each iteration
+    if string.find(source_line, "^" .. cwd) then
+        source_line = source_line:gsub("^" .. cwd .. "/", "") -- strip cwd from filename
+    end
+
+    -- to save space, look for home dir
+    --  i.e. /home/wes/foo/bar.lua
+    --    => ~/foo/bar.lua
+    local home = vim.fn.getenv("HOME")
+    if string.find(source_line, "^/") then
+        source_line = source_line:gsub(home, "~")
+    end
+
+    return source_line
+end
+
 function M.dump_globals()
     -- reminder... vim.print(_G) can be useful to inspect module state
     messages.ensure_open()
--- lua/devtools/nvim.lua
+++ lua/devtools/nvim.lua
@@ -1,5 +1,6 @@
 local messages = require('devtools.messages')
 local super_iter = require('devtools.super_iter')
+local lua = require('devtools.lua')
 
 local M = {}
 
@@ -82,53 +83,6 @@ function M.dump_keymaps(mode)
     messages.append(info or "No keymaps found")
 end
 
-local function inspect_fn_source_or_line(info)
-    local source_line = info.source .. ":" .. tostring(info.linedefined)
-    if not string.find(source_line, "^%@") then
-        -- return as is
-        return info.source
-    end
-    source_line = source_line:gsub("^@", "") -- strip '@' from filename
-
-    -- to save space, look for cwd
-    --  i.e. cwd = /foo/bar/
-    --       source = /foo/bar/bam/test.lua
-    --       =>       bam/test.lua
-    local cwd = vim.fn.getcwd()                               -- PRN pass cwd and home, if expensive to lookup on each iteration
-    if string.find(source_line, "^" .. cwd) then
-        source_line = source_line:gsub("^" .. cwd .. "/", "") -- strip cwd from filename
-    end
-
-    -- to save space, look for home dir
-    --  i.e. /home/wes/foo/bar.lua
-    --    => ~/foo/bar.lua
-    local home = vim.fn.getenv("HOME")
-    if string.find(source_line, "^/") then
-        source_line = source_line:gsub(home, "~")
-    end
-
-    return source_line
-end
-
-local function inspect_fn(fn)
-    -- debug return type: https://www.lua.org/manual/5.1/manual.html#3.8
-    local info = debug.getinfo(fn, "nS") -- n = name, S = source info
-    local what = info.what
-    if what == "Lua" then
-        -- not that useful to show "Lua"
-        what = nil
-    end
-    local source_or_line = inspect_fn_source_or_line(info)
-    -- BTW by logging entire object, its clear what is what, field wise...
-    --   AND, if a field is nil it won't take up any space!
-    return {
-        name = info.name,
-        what = what,      -- "Lua", "C", "main", etc.
-        func = info.func, -- name of function or nil
-        source_line = source_or_line
-    }
-end
-
 function M.dump_keymaps_sorted_by_lhs(mode, lhs_starts_with)
     mode = mode or "n"
     local header = "keymaps by lhs, mode: " .. mode
@@ -147,7 +101,7 @@ function M.dump_keymaps_sorted_by_lhs(mode, lhs_starts_with)
             rhs = m.rhs or m.callback
             if type(rhs) == "function" then
                 -- rhs = string.gsub(tostring(rhs), "%s+", "") -- trim whitespace in body
-                rhs = vim.inspect(inspect_fn(rhs))
+                rhs = vim.inspect(lua.inspect_fn(rhs))
             end
             m.sanitized_lhs = lhs
             m.sanitized_rhs = rhs
