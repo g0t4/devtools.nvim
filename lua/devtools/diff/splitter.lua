3c2132fbd3f62c408275b51771114ce5ab6204ee
Wes Higbee <wes.mcclure@gmail.com>
2025-08-08 05:26:05 -0500

    ok and now I need to bring back old logic for the existing tests that rely on consecutive spaces to be split together... for now that can stay separate and in time I can rewrite tests to be char by char OR write a separate set of tests... I do need to first discover if there are any issues with char by char split of seprators always

--- lua/devtools/diff/splitter.lua
+++ lua/devtools/diff/splitter.lua
@@ -3,12 +3,54 @@ local SPLIT_ON_WHITESPACE = '%s+'
 local STRIP_SEPARATORS = true
 local KEEP_SEPARATORS = false
 
+-----------------------------------------------------------------------------
+-- FYI this came from
+--     https://github.com/LuaDist/diff/blob/master/lua/diff.lua#L26
+--
+-- Split a string into tokens.  (Adapted from Gavin Kistner's split on
+-- http://lua-users.org/wiki/SplitJoin.
+--
+-- @param text           A string to be split.
+-- @param separator      [optional] the separator pattern (defaults to any
+--                       white space - %s+).
+-- @param skip_separator [optional] don't include the sepator in the results.
+-- @return               A list of tokens.
+-----------------------------------------------------------------------------
+local function split_internal(text, separator, skip_separator)
+    if separator == nil or separator == '' then
+        error('separator cannot be nil or empty string')
+    end
+
+    local parts = {}
+    local start = 1
+    local split_start, split_end = text:find(separator, start)
+    while split_start do
+        table.insert(parts, text:sub(start, split_start - 1))
+        if not skip_separator then
+            table.insert(parts, text:sub(split_start, split_end))
+        end
+        start = split_end + 1
+        split_start, split_end = text:find(separator, start)
+    end
+    if text:sub(start) ~= '' then
+        table.insert(parts, text:sub(start))
+    end
+    return parts
+end
+
+function M.split_on_whitespace_then_skip_the_whitespace(text)
+    return split_internal(text, SPLIT_ON_WHITESPACE, STRIP_SEPARATORS)
+end
+
+function M.split_on_whitespace(text)
+    return split_internal(text, SPLIT_ON_WHITESPACE, KEEP_SEPARATORS)
+end
 
 ---@param text string
 ---@param separator_pattern string
 ---@param skip_separator boolean # don't include the sepator in the results.
 ---@return table<string>
-local function split_internal(text, separator_pattern, skip_separator)
+local function split_sepators_char_by_char(text, separator_pattern, skip_separator)
     if separator_pattern == nil or separator_pattern == '' then
         error('separator cannot be nil or empty string')
     end
@@ -40,14 +82,6 @@ local function split_internal(text, separator_pattern, skip_separator)
     return parts
 end
 
-function M.split_on_whitespace_then_skip_the_whitespace(text)
-    return split_internal(text, SPLIT_ON_WHITESPACE, STRIP_SEPARATORS)
-end
-
-function M.split_on_whitespace(text)
-    return split_internal(text, SPLIT_ON_WHITESPACE, KEEP_SEPARATORS)
-end
-
 function M.split_code_into_words(text)
     -- goal here is to better split words in code to enhance diff comparisons
     --  i.e. if I have sse.choices[0].delta.content and I rename `sse` to `sse_parsed`
@@ -55,7 +89,7 @@ function M.split_code_into_words(text)
     --   AND then once underscore is included, you should simply see `_parsed` as added!
     --
     -- FYI goal is to use %W or [^%w] (not word char) after examples covered
-    return split_internal(text, "%W", KEEP_SEPARATORS)
+    return split_sepators_char_by_char(text, "%W", KEEP_SEPARATORS)
     -- return split_internal(text, "[._%s+*-/=\"\']", KEEP_SEPARATORS)
 end
 
