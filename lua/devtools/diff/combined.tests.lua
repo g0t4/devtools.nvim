a2c145c00905c584d7b806006edea0c58ed396c4
Wes Higbee <wes.mcclure@gmail.com>
2025-08-08 05:46:42 -0500

    switch test over to new splitter

--- lua/devtools/diff/combined.tests.lua
+++ lua/devtools/diff/combined.tests.lua
@@ -2,6 +2,7 @@ local histogram = require('devtools.diff.histogram')
 local should = require('devtools.tests.should')
 local combined = require('devtools.diff.combined')
 local _describe = require('devtools.tests._describe')
+local splitter = require('devtools.diff.splitter')
 
 -- TODO revisit this idea in devtools.tests and find a clean way to handle this
 --  perhaps add your own _it and _only? and override clear registraiton on an _only? and then on the only replace _it() func with nothing after that point...?
@@ -180,7 +181,7 @@ local pps = math.floor(sse_parsed.timings.predicted_per_second * 10 + 0.5) / 10
 print("tokens/sec", pps, "predicted_n", sse_parsed.timings.predicted_n)
 log:info("Tokens/sec: ", pps, " predicted n: ", sse_parsed.timings.predicted_n)]]
 
-        local combined_diff = combined.combined_diff(before_rename_text, after_rename_text)
+        local combined_diff = combined.combined_diff(before_rename_text, after_rename_text, splitter.split_code_into_words)
 
         local expected_groups = {
 
