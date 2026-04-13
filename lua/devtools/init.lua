062c194b558a699742563eba7d125f4a0aa95856
Wes Higbee <wes.mcclure@gmail.com>
2026-04-13 17:25:08 -0500

    add globals for devtools features that should be easy to access... at least for completions within :Dump/:Run command line input

--- lua/devtools/init.lua
+++ lua/devtools/init.lua
@@ -8,6 +8,27 @@ function M.setup(opts)
     messages.setup(opts)
     nvim.setup()
     lua.setup()
+
+    if opts.no_globals then
+        return
+    end
+
+
+    -- I wouldn't add these globally except that it helps with completions in Dump/Run commands
+    --   TODO long term I should augment just my two commands to have completions for the "globals" within those commands
+    --      (see env_overrides in dump_command)
+    --      for now, just hack the completions to work by providing these as globals in lua env everywhere
+    --      FYI this is only intended for running one-off snippets with Dump/Run (maybe :=/:lua too) not to get rid of requires in modules in my neovim config
+    _G.messages = messages
+    _G.inspect = require("devtools.inspect")
+    _G.append = messages.append
+    _G.ansi = require('devtools.ansi')
+    _G.bat_dump = messages.bat_dump
+    _G.bat_inspect = inspect.bat_inspect
+    --
+    -- idea:
+    --   :Run bat_dump({foo="bar"})
+    --   :Dump bat_i<TAB_COMPLETE>
 end
 
 return M
