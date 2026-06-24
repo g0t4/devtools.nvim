638ec3eec640af24b5c372ddb5bc69c07c62e5de
Wes Higbee <wes.mcclure@gmail.com>
2026-06-24 00:16:40 -0500

    it works! cool

--- lua/devtools/logs/fails.lua
+++ lua/devtools/logs/fails.lua
@@ -47,30 +47,32 @@ function M.load_to_quickfix_from_nvim()
     vim.cmd('copen')
 end
 
-function M.load_to_quickfix_from_hammerspoon()
+function load_to_quickfix_from_hammerspoon()
     host.throw_if_not_hammerspoon()
-    --test ... type fuck to nvim: use hs api to type
-    -- escape:
-    hs.eventtap.keyStroke({}, "escape") -- ensure normal mode
-    hs.eventtap.keyStrokes("ifuck") -- insert => fuck
 
-    do return end
-    -- forget about this for now:
-    local items = {}
-    for _, entry in ipairs(M.failures) do
-        table.insert(items, {
-            bufnr = entry.bufnr,
-            lnum = entry.line,
-            col = 0,
-            text = entry.traceback:match("[^\n]*") or "",
-        })
-    end
-    local json = hs.json.encode(items)
-    local script = string.format(
-        'tell application "Neovim" to execute "lua vim.fn.setqflist(%s, \'r\'); vim.cmd(\'copen\')"',
-        json
-    )
-    hs.osascript.applescript(script)
+    hs.eventtap.keyStroke({}, "escape") -- ensure normal mode
+    M.copy_last_failure()
+    hs.eventtap.keyStrokes(":copen\n") -- \n to submit <CR>
+    hs.eventtap.keyStrokes(":cgetexpr split(getreg('+'), '\\n')\n")
+
+    -- TODO is this a doable alternative?? gptoss FIM suggested this...
+    --  TODO so could I get PID of nvim instance => clipboard? by typing that command out to nvim current instance?
+    --  then use this to send trace to Neovim?
+    -- local items = {}
+    -- for _, entry in ipairs(M.failures) do
+    --     table.insert(items, {
+    --         bufnr = entry.bufnr,
+    --         lnum = entry.line,
+    --         col = 0,
+    --         text = entry.traceback:match("[^\n]*") or "",
+    --     })
+    -- end
+    -- local json = hs.json.encode(items)
+    -- local script = string.format(
+    --     'tell application "Neovim" to execute "lua vim.fn.setqflist(%s, \'r\'); vim.cmd(\'copen\')"',
+    --     json
+    -- )
+    -- hs.osascript.applescript(script)
 end
 
 -- test by uncommenting and then reload HS via streamdeck button while sitting in a nvim instance
