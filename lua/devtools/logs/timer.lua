b9a0150ab17f2896d8956a5a3ccb4c28d94a85c8
Wes Higbee <wes.mcclure@gmail.com>
2026-07-03 11:06:34 -0500

    drop asterisks off of long times... not always relevant, especially when tracking overall timing that alawys get into multi second territory and that itself does not mean something was excessively long

--- lua/devtools/logs/timer.lua
+++ lua/devtools/logs/timer.lua
@@ -44,13 +44,13 @@ end
 ---@return string
 function Timer.format_elapsed_time(elapsed_seconds)
     if elapsed_seconds >= 1 then
-        return string.format("** %.3f s **", elapsed_seconds)
+        return string.format("%.3f s", elapsed_seconds)
     end
     local ms = elapsed_seconds * 1e3
     if ms >= 1 then
         local time = string.format("%.2f ms", ms)
         if ms > 10 then
-            return "* " .. time .. " *"
+            return time
         end
         return time
     end
