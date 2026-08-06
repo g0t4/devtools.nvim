02f21715603439eac3c7cf18613a191e9fb6e475
Wes Higbee <wes.mcclure@gmail.com>
2026-08-05 22:39:39 -0500

    fix up tests for devtools repo

--- lua/devtools/traces/traces.tests.lua
+++ lua/devtools/traces/traces.tests.lua
@@ -1,9 +1,9 @@
-require("tests.setup").modify_package_path()
 local should = require('devtools.tests.should')
 local describe = require('devtools.tests.define.describe')
 local only = require('devtools.tests.define.only')
 local skip = require('devtools.tests.define.skip')
-local traces = require("helpers.traces")
+
+local traces = require("devtools.traces.traces")
 -- FYI changing lines below may mess up line numbers in assertion below for this file, just shift those for traces.tests.lua and it'll be fine!
 
 local function boom()
@@ -31,39 +31,39 @@ describe("resolve_truncated_path", function()
         local fixed_string = tostring(fixed)
 
         local home = vim.fn.getenv("HOME")
-        local expected = home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:10: boom
+        local expected = home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:10: boom
 stack traceback:
 	[C]: in function 'error'
-	]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:10: in function <]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:9>
+	]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:10: in function <]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:9>
 	[C]: in function 'xpcall'
-	]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:15: in function <]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:14>
+	]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:15: in function <]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:14>
 	[C]: in function 'xpcall'
 	]] .. home .. [[/.local/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:74: in function 'call_inner'
 	]] .. home .. [[/.local/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:175: in function 'it'
-	]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:14: in function <]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:13>
+	]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:14: in function <]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:13>
 	[C]: in function 'xpcall'
 	]] .. home .. [[/.local/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:74: in function 'call_inner'
 	]] .. home .. [[/.local/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:120: in function 'original_describe'
 	]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/tests/define/describe.lua:16: in function 'describe'
-	]] .. home .. [[/repos/github/g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:13: in function 'loaded'
+	]] .. home .. [[/repos/github/g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:13: in function 'loaded'
 	]] .. home .. [[/.local/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:239: in function <]] .. home .. [[/.local/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:238>]]
 
 
-        -- .../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:20: boom
+        -- .../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:20: boom
         -- stack traceback:
         --         [C]: in function 'error'
-        --         .../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:20: in function <.../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:19>
+        --         .../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:20: in function <.../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:19>
         --         [C]: in function 'xpcall'
-        --         .../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:60: in function <.../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:59>
+        --         .../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:60: in function <.../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:59>
         --         [C]: in function 'xpcall'
         --         ...ocal/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:74: in function 'call_inner'
         --         ...ocal/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:175: in function 'it'
-        --         .../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:59: in function <.../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:58>
+        --         .../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:59: in function <.../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:58>
         --         [C]: in function 'xpcall'
         --         ...ocal/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:74: in function 'call_inner'
         --         ...ocal/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:120: in function 'original_describe'
         --         ...0t4/devtools.nvim/lua/devtools/tests/define/describe.lua:16: in function 'describe'
-        --         .../g0t4/dotfiles/.config/nvim/lua/helpers/traces.tests.lua:58: in function 'loaded'
+        --         .../g0t4/devtools.nvim/lua/devtools/traces/traces.tests.lua:58: in function 'loaded'
         --         ...ocal/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:239: in function <...ocal/share/nvim/lazy/plenary.nvim/lua/plenary/busted.lua:238>
 
 
